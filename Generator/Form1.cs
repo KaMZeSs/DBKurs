5959f82f83487f1fb77c1ca0efc52c9085fc0a1d
KaMZeSs <vladd267@gmail.com>
2022-04-13 17:47:49 +0300

    commit

--- Generator/Form1.cs
+++ Generator/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace Generator
 {
@@ -15,6 +17,21 @@ namespace Generator
         public Form1()
         {
             InitializeComponent();
+            txtTOxml();
+        }
+
+        void txtTOxml()
+        {
+            using (var reader = new StreamReader("E:/Repos/DBKurs/Generator/Data/запись.txt"))
+            {
+                String[] r = reader.ReadToEnd().Split("\r\n");
+                Array.Sort(r);
+                var serializer = new XmlSerializer(typeof(string[]));
+                using (var writer = new StreamWriter("PropertyTypes.xml"))
+                {
+                    serializer.Serialize(writer, r);
+                }
+            }
         }
 
     }
